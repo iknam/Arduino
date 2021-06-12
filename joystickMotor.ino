47949f26ac2ddbe76fb2a779d1a8000899d2dbec
Il-kwon Nam <iknam86@gmail.com>
2021-06-12 21:34:24 +0900

    Add files via upload

--- joystickMotor.ino
+++ joystickMotor.ino
@@ -1,6 +1,10 @@
 /*
  * 2021. 06. 10
- * 조이스틱을 사용해 같은 방향 속도로 모터를 돌리기
+ * 조이스틱을 사용해 같은 방향 속도로 모터를 돌리기 수학적인 문제
+ * (0,1000) (0,500) (0,0)
+ * (500,1000) (500,500) (500,0)
+ * (1000,1000) (1000,500) (1000,0)
+ * 각 조이스틱 위치에서 서보모터 회전각
  */
 
 #include <Servo.h>
@@ -24,15 +28,20 @@ void loop() {
   // put your main code here, to run repeatedly:
   int x = analogRead(joy_x);
   int y = analogRead(joy_y);
-  int z = analogRead(joy_z);
+  //int z = analogRead(joy_z);
 
-  if(z = 0) servo.write(90);
-  if((x > 0 && x < 685) && (y > 0 && y < 685)){
-    value+=30;
+  //if(z = 0) servo.write(90);
+  if((x > 0 && x < 1000) && (y > 0 && y < 1028)){
+    value+=int(sqrt(pow(x, 2)+pow(y, 2)));
+    if(value > 360){
+      value %= 360;
+    }
     servo.write(value);
   }
   // x,y를 사용해서 서보모터 컨트롤
-  Serial.println(x);Serial.println(y);Serial.println(z);
+  Serial.print("x : ");Serial.println(x);
+  Serial.print("y : ");Serial.println(y);
+  Serial.print("value : ");Serial.println(value);
   
   delay(1000);
 }
